220c5e146c714ea19ba7586b3ac8f710c6d7288f
Ammaz <102025678+Ammaz1@users.noreply.github.com>
2022-03-28 19:11:06 +0500

    #28-Active Ragdoll

---
+++ Assets/Scripts/Test/MoveBaby.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBaby : MonoBehaviour
+{
+    public Transform BabyPickPoint;
+    public GameObject AnimBaby;
+    public Rigidbody Baby;
+
+    private bool BabyisPicked;
+
+    public Transform rotationObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BabyisPicked = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.J))
+        {
+            //BabyisPicked = true;
+            PickUpBaby(true);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            //BabyisPicked = false;
+            PickUpBaby(false);
+        }*/
+    }
+
+    public void PickUpBaby()
+    {
+        if (BabyisPicked)
+        {
+            AnimBaby.transform.parent = BabyPickPoint;
+            AnimBaby.transform.position = BabyPickPoint.position;
+            AnimBaby.transform.rotation = rotationObject.rotation;
+            Baby.isKinematic = true; 
+        }
+        else
+        {
+            AnimBaby.transform.parent = null;
+            Baby.isKinematic = false;
+            Baby.AddForce(transform.forward * 100f);
+            AnimBaby.transform.rotation = rotationObject.rotation;
+
+            Baby.drag = 2;
+        }
+        BabyisPicked = !BabyisPicked;  
+    }
+}
