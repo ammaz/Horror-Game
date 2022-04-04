7dca176723eaefe0e4969a35693da701d490b464
Ammaz <102025678+Ammaz1@users.noreply.github.com>
2022-04-04 16:00:35 +0500

    #34-Sound System

---
+++ Assets/Scripts/Sound/Sounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    public AudioClip Switch;
+    public static AudioSource src;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        src = GetComponent<AudioSource>();
+        Switch = GetComponent<AudioClip>();
+    }
+
+    public static void PlaySound(string clipName)
+    {
+        switch (clipName)
+        {
+            case "Switch":
+                //src.PlayOneShot(Switch);
+                break;
+        }
+    }
+}
